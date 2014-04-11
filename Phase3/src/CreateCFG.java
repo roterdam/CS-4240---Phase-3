3f465d110fcd2d2846607626f4143aed8e6b99e0
crystalPanter <crystalPanter@users.noreply.github.com>
2014-04-10 23:53:39 -0400

    Made some blocks
    And fixed som syntax

--- Phase3/src/CreateCFG.java
+++ Phase3/src/CreateCFG.java
@@ -9,13 +9,12 @@ import java.util.HashSet;
  */
 public class CreateCFG {
 
+	/**
+	 * testing
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// Let's do some shit! YEAH
-
-		/*
-		 * Beautiful testing low level IR code below I got it from the slide on
-		 * CFG analysis, so I'm assuming this is how all IR code input will be
-		 */
 		/*
 		 * String lowLevelIRCode =
 		 * "(1)   prod := 0\n(2)   i := 1\n(3)   t1 := 4 * i\n(4)   t2 := a[t1]\n"
@@ -23,8 +22,9 @@ public class CreateCFG {
 		 * "(8)   t6 := prod + t5\n(9)   prod := t6\n(10)   t7 := i + 1\n" +
 		 * "(11)   i := t7\n(12)   if i <= 20 goto (3)";
 		 */
-		
-		//this is assuming we take out any blank lines and leading/trailing white space
+
+		// this is assuming we take out any blank lines and leading/trailing
+		// white space
 		String highLevelIRCode = "assign, X, 100, 10\nassign, Y, 100, 10\nassign, i, 0,\nassign sum, 0,"
 				+ "\nmain:\nbrgeq, i, 100, end_loop\narray_load, t1, X, i\narray_load, t2, Y, i\nmult, t3, t1, t2\n"
 				+ "add, sum, sum, t3\nadd, i, i, 1\ngoto, main, ,\nend_loop:\ncall, printi, sum";
@@ -32,10 +32,64 @@ public class CreateCFG {
 		// So first things first, I have to identify the Leaders of the basic
 		// block
 		HashSet<String> leaders = findLeaders(highLevelIRCode);
+		// now we need to make the blocks
+		HashSet<CFGNode> blocks = buildBlocks(leaders, highLevelIRCode);
+	}
 
+	/**
+	 * This will build the basic blocks. The basic block corresponding to a
+	 * leader consists of the leader, plus all statements up to but not
+	 * including the next leader or up to the end of the program
+	 * 
+	 * @param leaders
+	 * @return
+	 */
+	private static HashSet<CFGNode> buildBlocks(HashSet<String> leaders,
+			String irCode) {
+		ArrayList<String> leaderList = new ArrayList<String>();
 		for (String each : leaders) {
-			System.out.println(each);
+			leaderList.add(each);
 		}
+
+		HashSet<CFGNode> blocks = new HashSet<CFGNode>();
+		String[] lines = irCode.split("\n");
+
+		for (int j = 0; j < leaders.size(); j++) { // for each leader
+			String blockCode = leaderList.get(j);
+			int found = 0;
+			for (int i = 1; i < lines.length; i++) { // for each line
+				// special case for the first leader
+				if (j == 0) {
+					if (!leaders.contains(lines[i])) {
+						blockCode = blockCode + "\n" + lines[i];
+					} else {
+						break;
+					}
+				}
+				// everyone else
+				if (found == 1) {	//if we are at the line after the leader
+					if(!leaders.contains(lines[i])){
+						blockCode = blockCode + "\n" + lines[i];
+					} else {
+						found=0;
+						break;
+					}
+				}
+				// go down to where the leader is first
+				if (leaderList.get(j).compareTo(lines[i]) == 0) {
+					found = 1;
+				}
+				// let it go
+
+			}
+
+			System.out.println(blockCode);
+			System.out.println("**********");
+
+			blocks.add(new CFGNode(blockCode));
+		}
+
+		return blocks;
 	}
 
 	/**
@@ -54,48 +108,48 @@ public class CreateCFG {
 		theLeaders.add(lines[0]); // OBEY rule 1
 
 		for (int i = 1; i < lines.length; i++) {
-				if (lines[i].contains("goto")) {
-					// OBEY rule 3
-					if ((i + 1) >= lines.length) {
-						// theLeaders.add("("+(lines.length+1)+")");
-						theLeaders.add("End of Code");
-					} else {
-						theLeaders.add(lines[i + 1]);
-					}
+			if (lines[i].contains("goto")) {
+				// OBEY rule 3
+				if ((i + 1) >= lines.length) {
+					// theLeaders.add("("+(lines.length+1)+")");
+					theLeaders.add("End of Code");
+				} else {
+					theLeaders.add(lines[i + 1]);
+				}
 
-					// Figure out where the goto is going to...
-					String[] goingTo = lines[i].split(","); // originally was
-															// split at goto
-					// char whatNumber = goingTo[1].charAt(1);
-					// theLeaders.add(lines[(Integer.parseInt(""+whatNumber))-1]);
-					// //OBEY rule 2
-					String where = goingTo[1].trim();
-					// find that spot
-					for (int j = 0; j < lines.length; j++) {
-						if (lines[j].contains(where + ":")) {
-							theLeaders.add(lines[j]); // OBEY rule2
-						}
+				// Figure out where the goto is going to...
+				String[] goingTo = lines[i].split(","); // originally was
+														// split at goto
+				// char whatNumber = goingTo[1].charAt(1);
+				// theLeaders.add(lines[(Integer.parseInt(""+whatNumber))-1]);
+				// //OBEY rule 2
+				String where = goingTo[1].trim();
+				// find that spot
+				for (int j = 0; j < lines.length; j++) {
+					if (lines[j].contains(where + ":")) {
+						theLeaders.add(lines[j]); // OBEY rule2
 					}
 				}
-				if (lines[i].contains("br")) {
-					// OBEY rule 3
-					if ((i + 1) >= lines.length) {
-						// theLeaders.add("("+(lines.length+1)+")");
-						theLeaders.add("End of Code");
-					} else {
-						theLeaders.add(lines[i + 1]);
-					}
+			}
+			if (lines[i].contains("br")) {
+				// OBEY rule 3
+				if ((i + 1) >= lines.length) {
+					// theLeaders.add("("+(lines.length+1)+")");
+					theLeaders.add("End of Code");
+				} else {
+					theLeaders.add(lines[i + 1]);
+				}
 
-					// Figure out where the br is going to...
-					String[] branch = lines[i].split(",");
-					String where = branch[3].trim();
-					// find that spot
-					for (int j = 0; j < lines.length; j++) {
-						if (lines[j].contains(where + ":")) {
-							theLeaders.add(lines[j]); // OBEY rule2
-						}
+				// Figure out where the br is going to...
+				String[] branch = lines[i].split(",");
+				String where = branch[3].trim();
+				// find that spot
+				for (int j = 0; j < lines.length; j++) {
+					if (lines[j].contains(where + ":")) {
+						theLeaders.add(lines[j]); // OBEY rule2
 					}
 				}
+			}
 		}
 		return theLeaders;
 	}
