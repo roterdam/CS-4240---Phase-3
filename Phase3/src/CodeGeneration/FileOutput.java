2845c307483fc3a88e5c5c94fce1e14d45f4605a
kwu70 <kwu70gt@gmail.coom>
2014-04-13 20:54:47 -0400

    file output

---
+++ Phase3/src/CodeGeneration/FileOutput.java
@@ -0,0 +1,19 @@
+package CodeGeneration;
+
+import java.io.IOException;
+
+public class FileOutput {
+	public static void main(String[] args) {
+		try(FileHandler fh = new FileHandler(args)){
+			CodeGeneration cg = new CodeGeneration();
+			fh.redirectConsole();
+			
+			for(Instructions i : cg.instList()){
+				System.out.println(i.toString());	
+			}
+
+		} catch(IOException e){
+			System.out.println("Error");
+		}
+	}
+}
