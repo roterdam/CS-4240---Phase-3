081ac9811aa3944caff234af563ff48b760b2bcc
crystalPanter <crystalPanter@users.noreply.github.com>
2014-04-22 13:06:39 -0400

    EBB Stuff

--- Phase3/src/EBB/EBBMethods.java
+++ Phase3/src/EBB/EBBMethods.java
@@ -31,15 +31,50 @@ public class EBBMethods {
 		ArrayList<EBBNode> nodes= new ArrayList<EBBNode>();
 		
 		CFGNode x;
-		ArrayList<CFGNode> s;
+		ArrayList<EBBNode> s = null;
 		
 		EbbRoots.add(r);
 
 		while(!EbbRoots.isEmpty()){
 			x = EbbRoots.poll();
-			//if()
+			int sInAllEbbs=0;
+			for(EBBNode each:s){
+				if(AllEbbs.contains(s)){
+					sInAllEbbs=1;
+				}
+				else{
+					sInAllEbbs=0;
+					break;
+				}
+			}
+			if(sInAllEbbs==1 && !s.get(0).equals(x)){
+				s.add(Build_Ebb(x,Succ,Pred));
+				AllEbbs.add(s);
+				}
 		}
 		return nodes;
 	}
+
+	private EBBNode Build_Ebb(CFGNode r, LinkedList<CFGNode> succ, CFGNode pred) {
+		EBBNode Ebb = new EBBNode();
+		Add_Bbs(r,Ebb,succ,pred);
+		return Ebb;
+	}
+
+	private void Add_Bbs(CFGNode r, EBBNode ebb, LinkedList<CFGNode> succ,
+			CFGNode pred) {
+		
+		CFGNode x = new CFGNode(null, 0);
+		ebb.addBlock(r);
+		for(CFGNode each: r.getNextBlock()){
+			if(pred.equals(each) && !ebb.getIRCode().contains(each.getIrCode())){
+				Add_Bbs(each, ebb, succ, pred);
+			}
+			else if(!EbbRoots.contains(each)){
+				EbbRoots.add(each);
+			}
+		}
+	}
+	
 
 }
